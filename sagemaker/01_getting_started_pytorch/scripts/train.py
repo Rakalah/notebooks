03e749905cda02ea37a29b6bb5f5f1daa58b4637
Rakalah <125032469+Rakalah@users.noreply.github.com>
2023-02-16 13:37:58 +0800

    Update train.py
    # gets role for executing training job
    role = sagemaker.get_execution_role()
    hyperparameters = {
    	'model_name_or_path':'dalle-mini/dalle-mini',
    	'output_dir':'/opt/ml/model'
    	# add your remaining hyperparameters
    	# more info here https://github.com/huggingface/transformers/tree/v4.17.0/examples/pytorch/token-classification
    }
    
    # git configuration to download our fine-tuning script
    git_config = {'repo': 'https://github.com/huggingface/transformers.git','branch': 'v4.17.0'}
    
    # creates Hugging Face estimator
    huggingface_estimator = HuggingFace(
    	entry_point='run_ner.py',
    	source_dir='./examples/pytorch/token-classification',
    	instance_type='ml.p3.2xlarge',
    	instance_count=1,
    	role=role,
    	git_config=git_config,
    	transformers_version='4.17.0',
    	pytorch_version='1.10.2',
    	py_version='py38',
    	hyperparameters = hyperparameters
    )
    
    # starting the train job
    huggingface_estimator.fit()

--- sagemaker/01_getting_started_pytorch/scripts/train.py
+++ sagemaker/01_getting_started_pytorch/scripts/train.py
@@ -93,3 +93,35 @@ if __name__ == "__main__":
 
     # Saves the model to s3
     trainer.save_model(args.model_dir)
+import sagemaker
+from sagemaker.huggingface import HuggingFace
+
+# gets role for executing training job
+role = sagemaker.get_execution_role()
+hyperparameters = {
+	'model_name_or_path':'dalle-mini/dalle-mini',
+	'output_dir':'/opt/ml/model'
+	# add your remaining hyperparameters
+	# more info here https://github.com/huggingface/transformers/tree/v4.17.0/examples/pytorch/token-classification
+}
+
+# git configuration to download our fine-tuning script
+git_config = {'repo': 'https://github.com/huggingface/transformers.git','branch': 'v4.17.0'}
+
+# creates Hugging Face estimator
+huggingface_estimator = HuggingFace(
+	entry_point='run_ner.py',
+	source_dir='./examples/pytorch/token-classification',
+	instance_type='ml.p3.2xlarge',
+	instance_count=1,
+	role=role,
+	git_config=git_config,
+	transformers_version='4.17.0',
+	pytorch_version='1.10.2',
+	py_version='py38',
+	hyperparameters = hyperparameters
+)
+
+# starting the train job
+huggingface_estimator.fit()
+
